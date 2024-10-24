53343b7a0afd20d411a2a869f28219ae25e414e2
InChul <49394875+inchori@users.noreply.github.com>
2024-10-25 00:11:29 +0900

    feat: add typeorm config

---
+++ src/config/typeorm.config.service.ts
@@ -0,0 +1,21 @@
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.configService.get<string>('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      username: this.configService.get<string>('DB_USER'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB_DATABASE'),
+      synchronize: false,
+      entities: [],
+    };
+  }
+}
