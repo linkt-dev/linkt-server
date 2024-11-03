64438dafe2c551c4cca2fef04e1bd82818a914f8
InChul <49394875+inchori@users.noreply.github.com>
2024-11-04 00:20:00 +0900

    fix

--- src/content/content.module.ts
+++ src/content/content.module.ts
@@ -3,9 +3,10 @@ import { ContentController } from './content.controller';
 import { ContentService } from './content.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Content } from './content.entity';
+import { MemberModule } from '../member/member.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Content])],
+  imports: [TypeOrmModule.forFeature([Content]), MemberModule],
   controllers: [ContentController],
   providers: [ContentService],
   exports: [ContentService],
