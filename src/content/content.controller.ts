bce2d06108f1e4197db92e74353f18b4130a4414
InChul <49394875+inchori@users.noreply.github.com>
2024-11-26 00:50:52 +0900

    feat: add authorization based on jwt passport (#8)

--- src/content/content.controller.ts
+++ src/content/content.controller.ts
@@ -1,33 +1,39 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import { ContentService } from './content.service';
 import { ContentCreateRequestDto } from './dto/content-create-request.dto';
 import { ContentResponseDto } from './dto/content-response.dto';
 import { ContentUpdateRequestDto } from './dto/content-update-request.dto';
 import { CommonResponseDto } from '../common/common-response.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { JwtRequest } from '../auth/types/jwt-request.interface';
 
 @Controller('contents')
 export class ContentController {
   constructor(private readonly contentService: ContentService) {}
 
   @Post()
-  async createContent(@Body() req: ContentCreateRequestDto) {
-    const content = await this.contentService.createContent(req.title, req.link, req.category, req.userId);
+  @UseGuards(AuthGuard('accessToken'))
+  async createContent(@Body() req: ContentCreateRequestDto, @Req() jwtRequest: JwtRequest) {
+    const content = await this.contentService.createContent(req.title, req.link, req.category, jwtRequest.user.userId);
     return ContentResponseDto.from(content);
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard('accessToken'))
   async getContent(@Param('id') id: number) {
     const content = await this.contentService.getContentById(id);
     return ContentResponseDto.from(content);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard('accessToken'))
   async updateContent(@Param('id') id: number, @Body() req: ContentUpdateRequestDto) {
     const updatedContent = await this.contentService.updateContent(id, req.title, req.link, req.category);
     return ContentResponseDto.from(updatedContent);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard('accessToken'))
   async deleteContent(@Param('id') id: number) {
     const deleteResult = await this.contentService.deleteContent(id);
     if (deleteResult.affected && deleteResult.affected > 0) {
