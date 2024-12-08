ea3f2512db56a2e4844771354fa235c2855473c8
InChul <49394875+inchori@users.noreply.github.com>
2024-12-08 23:10:20 +0900

    feat: update auth api response

--- src/auth/dto/login-response.dto.ts
+++ src/auth/dto/login-response.dto.ts
@@ -6,6 +6,11 @@ export class LoginResponseDto {
   })
   accessToken: string;
 
+  @ApiProperty({
+    type: 'string',
+  })
+  userId: string;
+
   static from(accessToken: string) {
     const responseDto = new LoginResponseDto();
     responseDto.accessToken = accessToken;
