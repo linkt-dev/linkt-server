bf86a471780a7f073b69457a0861452500a83c07
InChul <49394875+inchori@users.noreply.github.com>
2025-01-18 16:35:48 +0900

    feat: add dto class validator

--- src/auth/dto/login-request.dto.ts
+++ src/auth/dto/login-request.dto.ts
@@ -1,9 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class LoginRequestDto {
   @ApiProperty({
     type: 'string',
     required: true,
   })
+  @IsNotEmpty()
   userId: string;
 }
--- src/member/dto/member-create-request.dto.ts
+++ src/member/dto/member-create-request.dto.ts
@@ -1,9 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class MemberCreateRequestDto {
   @ApiProperty({
     type: 'string',
     required: true,
   })
+  @IsNotEmpty()
   uuid: string;
 }
